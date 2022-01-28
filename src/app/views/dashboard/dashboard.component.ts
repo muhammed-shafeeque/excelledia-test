b7146894e575d776405fe029529a8e52493eeb3b
shafeeqAGTS <muhammad.shafeek@agtstech.com>
2022-01-28 10:42:00 +0530

    grid section completed

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -8,8 +8,9 @@ import { MainService } from 'src/app/shared/api-service/main.service';
 })
 export class DashboardComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['position', 'name', 'status'];
+  dataSource = [];
+  allInfo: any;
   
   constructor(private service : MainService) { }
 
@@ -19,29 +20,11 @@ export class DashboardComponent implements OnInit {
 
   getAllList(){
     this.service.getAllList().subscribe(res =>{
-      console.log('lest',res)
-    })
+      this.allInfo = res;
+      this.allInfo = this.allInfo.data;
+      this.dataSource = this.allInfo;
+    });
   }
 
 }
 
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
